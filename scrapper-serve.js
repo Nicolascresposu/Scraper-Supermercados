b7c6b024b7e3970dbe21e43d1653cf3329663ae4
llas <nicolascresposuarez@gmail.com>
2025-08-12 12:53:16 -0400

    added basic scraping for amarket and fidalga

--- scrapper-serve.js
+++ scrapper-serve.js
@@ -34,7 +34,27 @@ let counter = 0;
 async function runScraperAmarket() {
 const URL = "https://amarket.com.bo/collections/all/products.json?page=" // Page size is 30.
 let endOfInventory = false;
-let counter = 0;
+let counter = 1; // La pagina 0 es igual a la pagina 1, asi que comenzamos en 1.
+while (!endOfInventory) {
+    const resp = await axios.get(URL+counter);
+    let products = resp.data?.products;
+    if (!products || products.length === 0) {
+        //This means we reached the end of a category
+        endOfInventory = true;
+        break;
+    }
+    for (const product of products) {
+        // Hay que extraer: nombre, vendor, tipo de producto, precio, link a imagen.
+        productName = product?.title
+        price = product?.variants[0]?.price
+        type = product?.product_type // Is ALWAYS empty on Amarket
+        vendor = product?.vendor
+        imageLink = product?.images[0]?.src
+        await sleep(20)
+    }
+    await sleep(2000)
+    counter+=1
+}
 }
 async function runScraperFidalga() {
 const URL = "https://www.fidalga.com/collections/all/products.json?page=" // Page size is 30.
@@ -49,7 +69,12 @@ while (!endOfInventory) {
         break;
     }
     for (const product of products) {
-        console.log(product?.title)
+        // Hay que extraer: nombre, vendor, tipo de producto, precio, link a imagen.
+        productName = product?.title
+        price = product?.variants[0]?.price
+        type = product?.product_type
+        vendor = product?.vendor
+        imageLink = product?.images[0]?.src
         await sleep(20)
     }
     await sleep(2000)
