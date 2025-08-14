9abebc0c587bc6b8a79ef8c048458c0cf6e6c488
llas <nicolascresposuarez@gmail.com>
2025-08-14 09:16:44 -0400

    First scrapper version completed

--- scrapper-serve.js
+++ scrapper-serve.js
@@ -4,11 +4,13 @@ const mysql = require('mysql2/promise');
 
 //Declaramos la funcion sleep que vamos a usar a lo largo del programa, y configuramos nuestra base de datos
 const dbConfig = {host: 'localhost', user: 'root', password: 'root', database: 'scraper_supermercados'};
-
+var now = new Date()
+now = now.toISOString().replace("T"," ").replace("Z","")
+console.log(now)
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 //Un switch para una variable que todavia no existe.
-let selection = 'hipermaxi'
+let selection = 'all'
 switch (selection) {
     case 'hipermaxi':
         runScraperHipermaxi();
@@ -29,9 +31,9 @@ switch (selection) {
 }
 async function insertToDatabase(arrayToInsert) {
     connection = await mysql.createConnection(dbConfig);
-    const sql = "INSERT INTO extraction (chain, vendor, date, name, sku, fullPrice, salePrice, discount) VALUES ?";
-    const values = arrayToInsert.map(p => [p.productName, p.price, p.vendor, p.productType,p.imageLink,p.supermarket]);
-    const [result] = await connection.query(sql, [values]);
+    const sql = "INSERT INTO extraccion (name, price, compareAtPrice, vendor, productType, imageLink, sku, supermarket, datetime) VALUES ?";
+    console.log(arrayToInsert)
+    const [result] = await connection.query(sql, [arrayToInsert]);
     console.log(`Success! Affected ${result.affectedRows}rows`)
 }
 async function runScraperHipermaxi() {
@@ -39,6 +41,7 @@ async function runScraperHipermaxi() {
     const URL = "https://hipermaxi.com/tienda-api/api/v1/public/productos?IdMarket=67&IdLocatario=67&Cantidad=500&Pagina=" //Most complex one.
     let endOfInventory = false;
     let counter = 1;
+    let allProducts = [];
     while (!endOfInventory) {
         const resp = await axios.get(URL+counter);
         let products = resp.data?.Dato;
@@ -51,24 +54,33 @@ async function runScraperHipermaxi() {
             // Hay que extraer: nombre, vendor, tipo de producto, precio, link a imagen.
             productName = product?.Descripcion
             price = product?.PrecioVenta
-            productType = product?.product_type // Is ALWAYS empty on Amarket
-            vendor = product?.vendor
-            imageLink = product?.images[0]?.src
-            // sku = product?.
+            compareAtPrice = product?.PrecioOriginal
+            if (compareAtPrice==0.0000 || !compareAtPrice) {
+                compareAtPrice = price
+            }
+            productType = "N/A" // Is ALWAYS empty on Amarket
+            vendor = "N/A"
+            imageLink = product?.UrlFoto
+            sku = product?.IdProducto
             supermarket = "Hipermaxi"
-            // await sleep(20)
-            productArray = [productName,price,productType,vendor,imageLink,supermarket]
-            // NEEDS duplicate protection.
+            productArray = [productName,price,compareAtPrice,vendor,productType,imageLink,sku,supermarket,now]
+            allProducts.push(productArray)
+            console.log(productArray)
+            // NEEDS duplicate protection. For later.
+            
         }
+        console.log(`Products added in Hipermaxi:${products.length} total: ${allProducts.length}`)
         await sleep(2000)
         counter+=1
     }
+    insertToDatabase(allProducts)
 }
 async function runScraperAmarket() {
     const URL = "https://amarket.com.bo/collections/all/products.json?page=" // Page size is 30.
     let endOfInventory = false;
     let counter = 1; // La pagina 0 es igual a la pagina 1, asi que comenzamos en 1.
-    while (!endOfInventory) {
+    let allProducts = [];
+    while (counter!=2) {
         const resp = await axios.get(URL+counter);
         let products = resp.data?.products;
         if (!products || products.length === 0) {
@@ -80,20 +92,29 @@ async function runScraperAmarket() {
             // Hay que extraer: nombre, vendor, tipo de producto, precio, link a imagen.
             productName = product?.title
             price = product?.variants[0]?.price
-            productType = product?.product_type // Is ALWAYS empty on Amarket
+            compareAtPrice = product?.variants[0]?.compare_at_price
+            if (!compareAtPrice || compareAtPrice == 0.00) {
+                compareAtPrice = price
+            }
             vendor = product?.vendor
+            productType = product?.product_type // Is ALWAYS empty on Amarket
             imageLink = product?.images[0]?.src
+            sku = product?.variants[0]?.sku
             supermarket = "Amarket"
-            productArray = [productName,price,productType,vendor,imageLink,supermarket]
+            productArray = [productName,price,compareAtPrice,vendor,productType,imageLink,sku,supermarket,now]
+            allProducts.push(productArray)
         }
+        console.log(`Products added in Amarket:${products.length} total: ${allProducts.length}`)
         await sleep(2000)
         counter+=1
     }
+    insertToDatabase(allProducts)
 }
 async function runScraperFidalga() {
     const URL = "https://www.fidalga.com/collections/all/products.json?page=" // Page size is 30.
     let endOfInventory = false;
     let counter = 1; // La pagina 0 es igual a la pagina 1, asi que comenzamos en 1.
+    let allProducts = []
     while (!endOfInventory) {
         const resp = await axios.get(URL+counter);
         let products = resp.data?.products;
@@ -106,13 +127,21 @@ async function runScraperFidalga() {
             // Hay que extraer: nombre, vendor, tipo de producto, precio, link a imagen.
             productName = product?.title
             price = product?.variants[0]?.price
-            productType = product?.product_type
+            compareAtPrice = product?.variants[0]?.compare_at_price
+            if (!compareAtPrice || compareAtPrice == 0.00) {
+                compareAtPrice = price
+            }
             vendor = product?.vendor
+            productType = product?.product_type // Is ALWAYS empty on Amarket
             imageLink = product?.images[0]?.src
-            supermarket = "Fidalga"
-            productArray = [productName,price,productType,vendor,imageLink,supermarket]
+            sku = product?.variants[0]?.sku
+            supermarket = "Amarket"
+            productArray = [productName,price,compareAtPrice,vendor,productType,imageLink,sku,supermarket,now]
+            allProducts.push(productArray)
         }
+        console.log(`Products added in Fidalga:${products.length} total: ${allProducts.length}`)
         await sleep(2000)
         counter+=1
     }
+    insertToDatabase(allProducts)
 }
