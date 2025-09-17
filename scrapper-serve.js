ef4b9ee0a6e2fd3973804b907bb2cbf8b297046e
llas <nicolascresposuarez@gmail.com>
2025-09-17 10:14:08 -0400

    actualizacion de readme

--- scrapper-serve.js
+++ scrapper-serve.js
@@ -61,9 +61,10 @@ while (!endOfInventory) {
         counter += 1;
     }
 }
+// Scraper para Fidalga, que utiliza el API de shopify expuesto
 async function runScraperFidalga() {
 const URL = "https://www.fidalga.com/collections/all/products.json?page=" // Page size is 30.
-let endOfInventory = false;
+let endOfInventory = false; //Variable we use 
 let counter = 1; // La pagina 0 es igual a la pagina 1, asi que comenzamos en 1.
 while (!endOfInventory) {
         const resp = await axios.get(URL + counter);
@@ -85,11 +86,11 @@ while (!endOfInventory) {
         counter += 1;
     }
 }
-
+// Scraper para Amarket, que utiliza el API de shopify expuesto
 async function runScraperAmarket() {
     const URL = "https://amarket.com.bo/collections/all/products.json?page="
-    let endOfInventory = false;
-    let counter = 1; 
+    let endOfInventory = false; //Variable we use 
+    let counter = 1; // La pagina 0 es igual a la pagina 1, asi que comenzamos en 1.
 
     while (!endOfInventory) {
         const resp = await axios.get(URL + counter);
@@ -161,7 +162,7 @@ cron.schedule('0 2 * * *', () => {
   mainFunction();
 }, {
   scheduled: true,
-  timezone: "America/New_York" // Example timezone, change to your own!
+  timezone: "America/New_York" // Example timezone, but works for us.
 });
 mainFunction();
 console.log('Scheduler started. Waiting for the scheduled time...');
